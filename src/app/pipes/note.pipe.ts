7785da069071cb20f7350da94bdda1e99560b5f0
Lilian <lilianouvmmi@gmail.com>
2021-02-08 09:55:41 +0100

    add stars notation for movies and series

--- src/app/pipes/note.pipe.ts
+++ src/app/pipes/note.pipe.ts
@@ -1,12 +1,25 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
+import { Pipe, PipeTransform, SecurityContext } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';  
 @Pipe({
   name: 'note'
 })
 export class NotePipe implements PipeTransform {
 
-  transform(value: number): unknown {
-    return null;
-  }
+  constructor(private sanitizer: DomSanitizer) { }
 
+  transform(note: number, maxNote: number): any {
+
+    let starsNoteHtml: string = '';
+    let starsMissingHtml: string = '';
+
+    for (let star = 0; star < Math.round(note); star++) {
+      starsNoteHtml += `<i class="fas fa-star"></i>`;
+    }
+
+    for (let star = 0; star < maxNote - Math.round(note); star++) {
+      starsMissingHtml += `<i class="far fa-star"></i>`;
+    }
+
+    return this.sanitizer.sanitize(SecurityContext.HTML, this.sanitizer.bypassSecurityTrustHtml(`${starsNoteHtml}${starsMissingHtml}`));
+  }
 }
